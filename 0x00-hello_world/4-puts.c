3f08ed2bf56fe752c69da80f1d87b68ebfa8b6c5
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-13 13:12:34 -0700

    First C program to print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main()
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
