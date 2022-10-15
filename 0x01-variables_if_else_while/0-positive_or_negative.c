26a301561aa37a0dc251e4e53781629628d074fa
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-15 03:22:14 -0700

    Get if the value of n is greater, lesser or equal to 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main header
  *
@@ -13,13 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	}else
 	{
-		printf("is negative\n");
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
