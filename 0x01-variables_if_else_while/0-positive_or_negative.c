252fc969a7cb4ca76f9243e925c8eabfe85af2dd
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-15 03:18:31 -0700

    Tell what the random number n is

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,5 +11,15 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}else if (n == 0)
+	{
+		printf("is zero\n");
+	}else
+	{
+		printf("is negative\n");
+	}
 	return (0);
 }
