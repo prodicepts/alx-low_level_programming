bd28902db9f17d8df5a6c1b2d867284aa14cd48f
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-15 03:29:36 -0700

    get if number n is greater, lesser or equal zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main header
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
  *
- * return (0)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
@@ -13,14 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	} else if (n == 0)
-	{
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	} else
-	{
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
