e395c6f809ed61a6da5bec05366a085fce697942
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-20 08:59:27 -0700

    up or down count to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - print from any number to 98 countdown or up.
@@ -5,21 +6,16 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
-		while (n >= 98)
-		{
-			_putchar(n--);
-		}
-	}
-	else if (n < 98)
-	{
-		while (n <= 98)
-		{
-			_putchar(n++);
-		}
+		while (n > 98)
+			printf("%d,", n--);
+		printf("\n");
 	}
 	else
-		_putchar(n);
-	_putchar('\n');
+	{
+		while (n < 98)
+			printf("%d,", n++);
+		printf("\n");
+	}
 }
