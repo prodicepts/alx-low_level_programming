3967629d05c88a9322b6aba1349a5ce831153b23
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-23 02:28:58 -0700

    print if argument is positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test function and print if integer
+ * is positive or negative
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
