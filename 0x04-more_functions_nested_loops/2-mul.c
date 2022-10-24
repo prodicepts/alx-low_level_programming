2a2d419660891927dda81956562b60282b93bc00
Shittu Monsuru o <s.monsur01@gmail.com>
2022-10-24 05:34:16 -0700

    products of two arguments

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: a*b  always
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
+
